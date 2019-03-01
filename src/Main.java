2f976322dc0154e7eda3d7b0446720bf3205c6e4
Arlecuno <palchinskij19@gmail.com>
2019-03-01 21:03:28 +0200

    Worked with bubble sort method.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,36 @@
 import arrays_test.RandomValues;
 import arrays_test.Worker;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
 
+        int[] array = {10, 100, 1, 42, 4, 8};
+        boolean isSort = false;
+        int change;
+        System.out.println("Before the sort " + Arrays.toString(array));
+        while (!isSort) {
+            isSort = true;
+            for (int i = 0; i < array.length - 1; i++) {
 
 
+                if (array[i] > array[i + 1]) {
+                    isSort = false;
 
 
+                    change = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = change;
 
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20 - i; j++) {
-                System.out.print(" ");
+                }
             }
-            for (int k = 0; k < (2 * i + 1); k++) {
-                System.out.print("*");
-            }
-            System.out.println();
         }
+        System.out.println("After the sort " + Arrays.toString(array));
+
+
 
 
 
